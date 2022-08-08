bd89e9b1126156e389b3bab5efec4d0aca6046a9
Tracy Derraco <ecert31@gmail.com>
2022-08-07 23:14:16 -0700

    asset caching

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,12 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(({ request }) => ['style', 'script', 'worker'].includes(repeat.destination),
+new StalewhileRevalidate({
+  cacheName: "asset-cache",
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    })
+  ]
+}));
